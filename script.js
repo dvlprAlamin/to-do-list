c57d74ddbe3f65b21ecfae517e50827d58f54e8e
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-02-02 13:24:17 +0600

    added confirmation

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function addList() {
     let input = document.getElementById('input-field');
     let todoList = document.getElementById('todo-list');
-    if(input.value == ''){
+    if (input.value === '') {
         return;
     }
-    let listItem = `<li> ${input.value} <span class="close-btn">✖</span> </li>`
+    let listItem = `<li> ${input.value} <span class="close-btn">✖</span> 
+    <div id="close-popup">
+    <h3>Are you sure to delete this item?</h3>
+    <button class="confirm-btn">Confirm</button>
+    <button class="popup-btn cancel-btn">Cancel</button>
+    </div></li>`
     // localStorage.setItem('listItem', listItem);
     // localStorage.getItem('listItem');
     todoList.innerHTML += listItem;
@@ -21,7 +26,12 @@ document.getElementById('input-field').addEventListener("keydown", function (eve
 
 document.getElementById('todo-list').addEventListener('click', function (event) {
     let item = event.target;
-    if (item.classList.value == 'close-btn') {
-        item.parentElement.style.display = "none"
+    if (item.classList.value === 'close-btn') {
+        // document.getElementById('close-popup').style.display = 'block'; 
+        item.nextElementSibling.style.display = 'block';       
     }
-})
+    if(item.classList.value === 'confirm-btn'){
+        item.parentElement.style.display = "none";
+        item.parentElement.parentElement.style.display = "none"
+    }
+});
