feb752ffe06e0cede65a706b7b568da1a6cff31a
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-02-05 00:43:16 +0600

    added local storage feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// add list item function
 function addList() {
     let input = document.getElementById('input-field');
     let todoList = document.getElementById('todo-list');
@@ -12,8 +13,16 @@ function addList() {
     </div></li>`
     todoList.innerHTML += listItem;
     input.value = '';
+    setLocalStorage(listItem);
 }
 
+
+// showing added list item on browser
+let getData = JSON.parse(localStorage.getItem('listItem'));
+document.getElementById('todo-list').innerHTML = getData.join('');
+
+
+// enter key event handler
 document.getElementById('input-field').addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
@@ -22,11 +31,20 @@ document.getElementById('input-field').addEventListener("keydown", function (eve
 });
 
 
+// delete, confirm, cancel button event handler
 document.getElementById('todo-list').addEventListener('click', function (event) {
     let item = event.target;
     if (item.classList.value === 'close-btn') {
-        // document.getElementById('close-popup').style.display = 'block'; 
         item.nextElementSibling.style.display = 'block';
+        // finding index of list item which need to remove from local storage
+        let child = item.parentElement;
+        let i = 0;
+        while ((child = child.previousSibling) != null)
+            i++;
+        // delete selected list item from local storage
+        let fetchedData = JSON.parse(localStorage.getItem("listItem"));
+        fetchedData.splice(i, 1);
+        localStorage.setItem('listItem', JSON.stringify(fetchedData));
     }
     if (item.classList.value === 'confirm-btn') {
         item.parentElement.style.display = "none";
@@ -36,3 +54,18 @@ document.getElementById('todo-list').addEventListener('click', function (event) 
         item.parentElement.style.display = "none";
     }
 });
+
+
+// set list item to local storage 
+function setLocalStorage(data) {
+    let availableData = localStorage.getItem('listItem');
+    if (availableData === null) {
+        localStorage.setItem('listItem', JSON.stringify([data]));
+    }
+    else {
+        fetchedData = JSON.parse(availableData);
+        fetchedData.push(data);
+        localStorage.setItem('listItem', JSON.stringify(fetchedData));
+
+    }
+};
